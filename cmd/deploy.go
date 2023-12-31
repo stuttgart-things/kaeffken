c7356d506f73d181216cfb519d43bc328f3195b6
Patrick Hermann <patrick.hermann@sva.de>
2023-12-31 16:56:40 +0000

    initial commit of sceleton

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -1,6 +1,5 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2023 PATRICK HERMANN PATRICK.HERMANN@SVA.DE
 */
 package cmd
 
@@ -13,28 +12,18 @@ import (
 // deployCmd represents the deploy command
 var deployCmd = &cobra.Command{
 	Use:   "deploy",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "create and deploy gitops apps",
+	Long:  `create and deploy gitops applications.`,
 	Run: func(cmd *cobra.Command, args []string) {
+
+		// GET ENV VARS, CLUSTER VARS AND APP CATALOUGE
+		// repo, _ := sthingsCli.CloneGitRepository(gitRepository, gitBranch, gitCommitID, nil)
+		// profileFile = sthingsCli.ReadFileContentFromGitRepo(repo, profile)
 		fmt.Println("deploy called")
+
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(deployCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// deployCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// deployCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
