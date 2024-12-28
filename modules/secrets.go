11cba41a85cd6f863f0893e6e7266094fde1bb89
Patrick Hermann <patrick.hermann@sva.de>
2024-12-28 07:00:25 +0000

    added secrets generator

---
+++ modules/secrets.go
@@ -0,0 +1,46 @@
+/*
+Copyright © 2024 PATRICK HERMANN PATRICK.HERMANN@SVA.DE
+*/
+
+package modules
+
+import (
+	"fmt"
+
+	"gopkg.in/yaml.v3"
+)
+
+func CreateSecretsMap(yamlData []byte, wantedSecrets map[string]interface{}) (secrets map[string]interface{}) {
+
+	secrets = make(map[string]interface{})
+
+	// CREATE A VARIABLE TO HOLD THE PARSED DATA
+	var data map[string]interface{}
+
+	// PARSE THE YAML
+	err := yaml.Unmarshal([]byte(yamlData), &data)
+	if err != nil {
+		log.Fatalf("ERROR PARSING YAML: %v", err)
+	}
+
+	// CREATE THE SECRETS MAP
+	for key, value := range data {
+		fmt.Println("KEYY", key)
+
+		switch v := value.(type) {
+		case map[string]interface{}:
+			for k, v := range v {
+
+				// CHECK IF THE KEY IS IN THE WANTED SECRETS
+				if _, ok := wantedSecrets[k]; ok {
+					secrets[wantedSecrets[k].(string)] = v
+				}
+
+				fmt.Println(k, v)
+			}
+		}
+
+	}
+
+	return
+}
