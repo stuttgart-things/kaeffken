61d799ebf1d6784aea11c4f4c4e4090f6e73d4e6
Patrick Hermann <patrick.hermann@sva.de>
2024-12-30 09:36:57 +0000

    updated homerun test

--- modules/homerun.go
+++ modules/homerun.go
@@ -5,6 +5,7 @@ package modules
 
 import (
 	"bytes"
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -13,38 +14,44 @@ import (
 	homerun "github.com/stuttgart-things/homerun-library"
 )
 
-func SendMessageToHomerun(homerunURL, authToken string, message homerun.Message) (err error, responsCode string) {
-
+func SendMessageToHomerun(homerunURL, authToken string, message homerun.Message) (err error, responseCode string) {
 	// MARSHAL THE MESSAGE STRUCT TO JSON
 	payloadBytes, err := json.Marshal(message)
 	if err != nil {
 		fmt.Printf("Error marshaling JSON: %v\n", err)
-		return
+		return err, ""
 	}
 
 	// CREATE THE HTTP REQUEST
 	req, err := http.NewRequest("POST", homerunURL, bytes.NewBuffer(payloadBytes))
 	if err != nil {
 		fmt.Printf("Error creating request: %v\n", err)
-		return
+		return err, ""
 	}
 
 	// SET HEADERS
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("X-Auth-Token", authToken)
 
-	// CREATE AN HTTP CLIENT AND MAKE THE REQUEST
-	client := &http.Client{Timeout: 10 * time.Second}
+	// CREATE AN HTTP CLIENT THAT IGNORES SSL CERTIFICATE VERIFICATION
+	customTransport := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true}, // Disable SSL verification
+	}
+	client := &http.Client{
+		Timeout:   10 * time.Second,
+		Transport: customTransport,
+	}
+
+	// MAKE THE REQUEST
 	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Printf("Error making request: %v\n", err)
-		return
+		return err, ""
 	}
 	defer resp.Body.Close()
 
 	// PRINT THE RESPONSE STATUS
 	fmt.Printf("Response status: %s\n", resp.Status)
 
-	return err, resp.Status
-
+	return nil, resp.Status
 }
