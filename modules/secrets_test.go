2d4db1ff25a16f0246b0047f24318431869ac968
Patrick Hermann <patrick.hermann@sva.de>
2025-01-01 13:14:51 +0000

    updadte secret map function

--- modules/secrets_test.go
+++ modules/secrets_test.go
@@ -10,14 +10,20 @@ import (
 
 func TestCreateSecretsMap(t *testing.T) {
 
-	yamlData := `
+	flatYAML := `
+host: localhost
+port: 5432
+username: dbuser
+password: dbpassword
+name: example_db`
+
+	dictYAML := `
 secrets:
   host: localhost
   port: 5432
   username: dbuser
   password: dbpassword
-  name: example_db
-`
+  name: example_db`
 
 	// NEEDED SECRETS
 	wantedSecrets := map[string]interface{}{
@@ -25,7 +31,10 @@ secrets:
 		"port": "portNumber",
 	}
 
-	secrets := CreateSecretsMap([]byte(yamlData), wantedSecrets)
-	fmt.Println(secrets)
+	secretsFlat := CreateSecretsMap([]byte(flatYAML), wantedSecrets)
+	fmt.Println("RESULT flatYAML", secretsFlat)
+
+	secretsDict := CreateSecretsMap([]byte(dictYAML), wantedSecrets)
+	fmt.Println("RESULT dictYAML", secretsDict)
 
 }
